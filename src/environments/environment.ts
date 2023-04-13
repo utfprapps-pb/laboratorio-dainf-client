d985afb0b842ff0d384497f86507aeab2a2df0b6
Vinicius Pegorini <viniciuspegorini@gmail.com>
2023-04-13 19:43:58 -0300

    test deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   //api_url: 'https://tcc-server-utfpr.herokuapp.com/server/'
   //api_url: 'http://172.20.190.22:8099/server/'
-  api_url: 'http://localhost:8099/server/',
+  // api_url: 'http://localhost:8099/server/',
+  api_url: 'https://dainf-labs-api.app.pb.utfpr.edu.br/',
   google_client_id: '310109923674-la5thl4s4t0b2ajp6acdhq7tra74dn31.apps.googleusercontent.com'
 };
 
